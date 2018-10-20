7e0abf6510b26960ce0058dae9e00a96bc5ff93c
Bruce Nelson <bnelson.rugger@gmail.com>
2018-10-20 14:13:44 -0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace automate2
+{
+    public class HelloWorld
+    {
+        public static string Start()
+        {
+            return "Hello World at " + DateTime.Now.ToString();
+        }
+
+        public static string SayHello(String whoisit )
+        {
+            return "Howdy " + whoisit.ToString();
+        }
+        public static string Stop()
+        {
+            return "all done";
+        }
+
+        public static string All()
+        {
+            String foo = Start();
+            String bar = Stop();
+            return $"finished Start: {foo} Stop : {bar}";
+        }
+    }
+}
